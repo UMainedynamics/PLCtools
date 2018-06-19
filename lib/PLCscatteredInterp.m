a9e0e6b71cd37b46a899de74e020388f0a649935
NWRichmond <nicholaswrichmond@gmail.com>
2018-06-19 16:08:27 -0400

    Added phase masking for non-square data
    The grain boundary (normalized to 0 -> 1 space) provided by ms.Grains provides the basis for creating a polygon inside which data is excluded by converting results to NaN.

--- lib/PLCscatteredInterp.m
+++ lib/PLCscatteredInterp.m
@@ -4,34 +4,44 @@ cd(strcat('C:\Users\', ...
     user,'\Documents\GitHub\PLCtools\', ...
     'DEMO_DATA\',plc_case));
 load('Fold3cgsm30.mat')
+resolution = 100;
+phase_to_exclude = 2;
+[row,~] = find(ms.GrainPhases == phase_to_exclude);
+[row_elem,~] = find(ms.ElementPhases == phase_to_exclude);
+
 coords = ms.MicroCoordinates/max(ms.MicroCoordinates(:));
 mst =[coords, ms.MicroStress{10, 1}(:,end)];
 mst_table = array2table(mst,...
     'VariableNames',{'x','y','stress'});
+mst_mask = mst(row_elem,:);
+mst_mask_table = array2table(mst_mask,...
+    'VariableNames',{'x','y','stress'});
+
 F = scatteredInterpolant(mst_table.x,mst_table.y,mst_table.stress);
-[xq,yq] = meshgrid(linspace(0,1,100),linspace(0,0.75,100));
+max_x = max(mst_table.x);
+max_y = max(mst_table.y);
+[xq,yq] = meshgrid(linspace(0,max_x,max_x*resolution), ...
+    linspace(0,max_y,max_x*resolution));
 cq = F(xq,yq);
 
-phase_to_exclude = 2;
-[row,~] = find(ms.GrainPhases == phase_to_exclude);
 xGrain = [ms.Grains{1, row}(:,1),ms.Grains{1, row}(:,2)];
 xGrainNorm = xGrain/max(xGrain(:));
-
 xv = xGrainNorm(:,1);
 yv = xGrainNorm(:,2);
-px = polyshape(xGrainNorm);
-[x, y] = boundary(px);
-k = boundary(x, y);
-[in,on] = inpolygon(xq,yq,xv(k),yv(k));
-%nullpolygon = [xv(k),yv(k)];
+[in,on] = inpolygon(xq,yq,xv,yv);
 cq(in) = NaN;
 cq(on) = NaN;
 
 h = pcolor(xq,yq,cq);
 h.EdgeColor = 'none';
 
-hold on
-scatter(xGrainNorm(:,1),xGrainNorm(:,2))
+% hold on
+% px = polyshape(xGrainNorm,'Simplify',true);
+% plot(px)
+
+
+% scatter(mst_mask(:,1),mst_mask(:,2))
+% scatter(xGrainNorm(:,1),xGrainNorm(:,2))
 
 % hold on
 % scatter(ms.Grains{1, 1}(:,1),ms.Grains{1, 1}(:,2))
